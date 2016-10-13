9c61258945c9234542716f5c2cfb4cf68c82cb15
Sandeep Raju Prabhakar <SandeepPrabhakar2015@u.northwestern.edu>
2016-10-13 18:20:12 -0500

    adds enable, disable, trigger flags

--- cli.py
+++ cli.py
@@ -8,9 +8,19 @@ def run():
     functionalities.
     """
     parser = argparse.ArgumentParser(description='gittalk')
-    args = parser.parse_args()
-    print 'Hello Git Talk!'
 
+    parser.add_argument('-e', '--enable', action='store_true', required=False,
+                        help='Enable Git Talk in the current Git repository.',
+                        dest='enable')
+    parser.add_argument('-d', '--disable', action='store_true', required=False,
+                        help='Disable Git Talk in the current Git repository.',
+                        dest='disable')
+    parser.add_argument('-t', '--trigger', action='store_true', required=False,
+                        help='Trigger Git Talk.',
+                        dest='trigger')
+    args = parser.parse_args()
+
+    print 'hello git talk!'
 
 if __name__ == '__main__':
     run()
---
+++ gittalk/gittalk.py
@@ -0,0 +1,12 @@
+class GitTalk(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def enable(self):
+        pass
+
+    def disable(self):
+        pass
+
+    def trigger(self):
+        pass
