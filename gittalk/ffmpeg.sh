2149865c8f7fc4231f3221e274d26776ff201292
Malek Assi <malekassi2212@gmail.com>
2016-10-20 20:01:08 -0500

    test3 Uploading file... Video id 'hQXisW4SoAU' was successfully uploaded. https://www.youtube.com/watch?v=hQXisW4SoAU None

--- gittalk/ffmpeg.sh
+++ gittalk/ffmpeg.sh
@@ -6,9 +6,9 @@
 # screengrab still has bad audio, trying to figure that one out
 
 arg=$1
-audio="ffmpeg -f avfoundation -i \"none:0\" -t 00:00:05 -y -vn -f mp4 -c:a libmp3lame output.mp4"
-video="ffmpeg -f avfoundation -r 30 -c:v rawvideo -i \"0:0\" -t 00:00:05 -y -f mp4 -c:v mpeg4 -c:a libmp3lame output.mp4"
-screengrab="ffmpeg -f avfoundation -r 30 -c:v rawvideo -i \"1:0\" -t 00:00:05 -y -f mp4 -c:v mpeg4 -c:a libmp3lame output.mp4"
+audio="ffmpeg -f avfoundation -i \"none:0\" -t 00:00:05 -y -vn -f mp4 -c:a aac output.mp4"
+video="ffmpeg -f avfoundation -r 30 -c:v rawvideo -i \"0:0\" -t 00:00:05 -y -f mp4 -c:v mpeg4 -c:a aac output.mp4"
+screengrab="ffmpeg -f avfoundation -r 30 -c:v rawvideo -i \"1:0\" -t 00:00:05 -y -f mp4 -c:v mpeg4 -c:a aac output.mp4"
 
 if [ "$arg" == "a" ]; then
 	eval $audio
