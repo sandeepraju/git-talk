548104429f474e9ffbd8da6687d7d15184bdf379
yannick-mamudo <yannick.mamudo@gmail.com>
2016-10-20 19:28:50 -0500

    fix enable/disable functionality

--- gittalk/enable.py
+++ gittalk/enable.py
@@ -4,40 +4,42 @@ Module for enabling and disabling git talk
 
 import os
 
-class HookInstaller(object):
+class HookInstaller:
     """ Class for all enable and disable methods """
 
-    # Global var
-    pwd = os.getcwd()
-    git_talk_head = "#### Git Talk (begin)"
-    git_talk_tail = "#### Git Talk (end)"
+    def __init__(self, git_r):
+        self.git_root = git_r
+        self.git_talk_head = "##### gittalk hook (begin) #####\n"
+        self.git_talk_tail = "##### gittalk hook (end) #####\n"
 
     def isGitInitialized(self):
         ''' Checks whether the project has a git project initialized '''
-        git_folder = os.path.abspath("".join((self.pwd, '/.git')))
+        git_folder = os.path.abspath("".join((self.git_root, '/.git')))
         return os.path.isdir(git_folder)
 
     def isHookInitialized(self):
         ''' Checks whether the project has a git project initialized '''
-        hook_file = os.path.abspath("".join((self.pwd, '/.git/hooks/post-commit')))
+        hook_file = os.path.abspath("".join((self.git_root, '/.git/hooks/commit-msg')))
         return os.path.isfile(hook_file)
 
     def addHook(self):
-        ''' Looks into .git/hooks to add hook from post-commit '''
+        ''' Looks into .git/hooks to add hook from commit-msg '''
         if self.isGitInitialized():
             if self.isHookInitialized():
-                hook_file = open("".join((self.pwd, '/.git/hooks/post-commit')), 'r+')
+                hook_file = open("".join((self.git_root, '/.git/hooks/commit-msg')), 'r+')
             else:
-                hook_file = open("".join((self.pwd, '/.git/hooks/post-commit')), 'w+')
+                hook_file = open("".join((self.git_root, '/.git/hooks/commit-msg')), 'w+')
+                hook_file.write("#!/bin/sh\n")
+
 
             # look for git commit head
             hook_content = hook_file.read()
             head_loc = hook_content.find(self.git_talk_head)
 
-            # writes to post-commit
+            # writes to commit-msg
             if head_loc == -1:
                 hook_file.seek(0, 2)
-                with open("githook.txt", "r") as hook:
+                with open(os.path.abspath("./gittalk/assets/hooks/commit-msg"), "r") as hook:
                     for line in hook:
                         hook_file.write(line)
             else:
@@ -46,20 +48,22 @@ class HookInstaller(object):
             print("Not in a git root folder")
 
     def rmHook(self):
-        ''' Looks into .git/hooks to remove hook from post-commit '''
+        ''' Looks into .git/hooks to remove hook from commit-msg '''
         if self.isGitInitialized():
             if self.isHookInitialized():
-                hook_file = open("".join((self.pwd, '/.git/hooks/post-commit')), 'r+')
+                hook_file = open("".join((self.git_root, '/.git/hooks/commit-msg')), 'r+')
 
                 # look for git commit head
                 hook_content = hook_file.read()
                 head_loc = hook_content.find(self.git_talk_head)
-                hook_file.close()
 
-                # writes to post-commit
-                if head_loc == -1:
-                    hook_file = open("".join((self.pwd, '/.git/hooks/post-commit')), 'w')
+                # writes to commit-msg
+                if head_loc != -1:
+                    hook_file.seek(0)
                     hook_content = hook_file.readlines()
+                    ## reopen for rewrite
+                    hook_file = open("".join((self.git_root, '/.git/hooks/commit-msg')), 'w')
+
                     omit = False
                     deletedHook = False
                     for line in hook_content:
@@ -67,17 +71,17 @@ class HookInstaller(object):
                             ## stops checking for the hook
                             hook_file.write(line)
                         elif omit:
-                            if line.find(self.git_talk_tail)!=-1:
+                            if line == self.git_talk_tail:
                                 ## found the tail
                                 omit = False
                                 deletedHook = True
                         else:
-                            if line.find(self.git_talk_tail)!=-1:
+                            if line == self.git_talk_head:
                                 ## found the head
                                 omit = True
                             else:
                                 hook_file.write(line)
             else:
-                print("post-commit does not exist")
+                print("commit-msg does not exist")
         else:
             print("Not in a git root folder")
--- gittalk/gittalk.py
+++ gittalk/gittalk.py
@@ -7,21 +7,26 @@ from enable import HookInstaller
 
 class GitTalk(object):
     def __init__(self, *args, **kwargs):
+        self.git_root = get_git_root(os.getcwd())
+        self.installer = HookInstaller(self.git_root)
         pass
 
     def enable(self):
         try:
             # git_root = get_git_root(os.getcwd())
-            # commit_msg_hook = os.path.join(git_root, '.git', 'hooks', 'commit-msg')
-            # write_hook(commit_msg_hook)
-            HookInstaller.addHook()
+            # # commit_msg_hook = os.path.join(git_root, '.git', 'hooks', 'commit-msg')
+            # # write_hook(commit_msg_hook)
+            # installer = HookInstaller(git_root)
+            self.installer.addHook()
             print 'GitTalk enabled successfully!'
         except Exception as e:
             print 'GitTalk cannot be enabled. {}'.format(e.message)
 
     def disable(self):
         try:
-            HookInstaller.rmHook()
+            # git_root = get_git_root(os.getcwd())
+            # installer = HookInstaller(git_root)
+            self.installer.rmHook()
             print 'GitTalk disabled successfully!'
         except Exception as e:
             print 'GitTalk cannot be disabled. {}'.format(e.message)
