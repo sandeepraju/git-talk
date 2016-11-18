36c6301d75fd84097a8f5273e0c14b22549440d9
Sandeep Raju Prabhakar <SandeepPrabhakar2015@u.northwestern.edu>
2016-11-17 18:46:34 -0600

    adds holmebrw formula

---
+++ gittalk.rb
@@ -0,0 +1,41 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Gittalk < Formula
+  desc "338"
+  homepage "https://github.com/sandeepraju/git-talk"
+  url "https://github.com/sandeepraju/git-talk/raw/master/gittalk.tar.gz"
+  sha256 "b49d5a3d5cc2b5c63cf576e6d919252705fa1aa8edf1cc03377a92189c975e7e"
+  version '0.9'
+
+  # depends_on "cmake" => :build
+#  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+#    system "./configure", "--disable-debug",
+#                          "--disable-dependency-tracking",
+#                          "--disable-silent-rules",
+#                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+#    system "bash"
+    system "make", "build" # if this fails, try separate make/make install steps
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test gittalk`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "gittalk"
+  end
+end
