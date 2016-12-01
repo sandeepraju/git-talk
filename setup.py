4d8fe6bcf4d7f2e66e9b1d9c48d024b295576abb
Sandeep Raju Prabhakar <sandeep080@gmail.com>
2016-12-01 17:02:49 -0600

    bumps up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='git-talk',
-    version='0.0.8a0',
+    version='0.0.9a0',
     description='Audio & video annotations for your code with Git!',
     long_description=('For more information, visit:'
                       ' https://github.com/sandeepraju/git-talk'),
